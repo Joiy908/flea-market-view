829b467d777612c8f451870144f6264efbdc50c2
joiy908 <joiy9088@gmail.com>
2023-04-21 22:22:02 +0800

    set env

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,19 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+    <van-button type="primary">主要按钮</van-button>
+    <van-button type="info">信息按钮</van-button>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    // HelloWorld
   }
 }
 </script>
--- src/main.js
+++ src/main.js
@@ -1,7 +1,10 @@
 import Vue from 'vue'
 import App from './App.vue'
+import { Button } from 'vant'
 
 Vue.config.productionTip = false
+
+Vue.component(Button.name, Button)
 
 new Vue({
   render: h => h(App),
